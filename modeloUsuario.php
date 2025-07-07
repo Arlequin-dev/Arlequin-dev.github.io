4f190b4d75778b48f6f33b21106322410c38bb62
Arlequin-dev <devarlequin@gmail.com>
2025-07-07 00:37:32 -0300

    Add files via upload

--- modeloUsuario.php
+++ modeloUsuario.php
@@ -5,7 +5,8 @@ class ModeloUsuario
 
     public function __construct()
     {
-        $this->db = new mysqli("localhost", "root", "", "prueba");
+        $this->db = new mysqli("mi_mysql", "usuario", "password123", "prueba");
+
     }
 
     public function insertarUsuario($nombre,$rol, $email, $pswd, $tel)
